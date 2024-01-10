47887c955207c7d43617d43152b22a4f2fec1ab8
Ayyan-Faraz <155670123+Ayyan-Faraz@users.noreply.github.com>
2024-01-10 19:40:10 +0500

    average.py

--- average.py
+++ average.py
@@ -3,4 +3,4 @@ print(a)
 num1=float(input("Enter First Floating point number:   "))
 num2=float(input("Enter Second Floating point number:   "))
 avg=(num1+num2)/2
-print("the average of two numeber is:  ",avg)
+print(f"the average of two numeber is:{avg:.2f}")
